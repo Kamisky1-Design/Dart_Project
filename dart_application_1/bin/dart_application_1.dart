c5babfa6f95df7a05a3d9e3ae23926cf00fc1f19
Komla Sanussi <your.kamilousanussi@gmail.com>
2026-03-15 20:21:33 +0000

    Use of String interpolation

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,8 @@
 void main() {
-  int studentId = 1234; // variable declaration
+  int studentId = 1234; // variable declaratio
   print(studentId);
   
+  var yearOfStudies = 4;  // use of String interpolation
+  int yearOfStudy = 2; 
+  print('yearOfStudies: ${yearOfStudies % yearOfStudy} years');
 }
