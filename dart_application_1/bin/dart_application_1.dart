daab133e117a9acf7755674ac9cd596ca17215a5
Komla Sanussi <your.kamilousanussi@gmail.com>
2026-03-15 21:30:01 +0000

    If else statement in dart

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -5,4 +5,13 @@ void main() {
   var yearOfStudies = 4;  // use of String interpolation
   int yearOfStudy = 2; 
   print('yearOfStudies: ${yearOfStudies % yearOfStudy} years');
+
+
+  double soilMoisture = 50.0; // if else statatement
+if (soilMoisture < 40) {
+print('Soil is too dry. Start irrigation.');
+} else {
+print('Soil moisture is fine. No irrigation needed.');
+}
+
 }
