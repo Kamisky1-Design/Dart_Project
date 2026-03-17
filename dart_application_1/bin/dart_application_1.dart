9af03d6a7121755c6519216da12e50a21334fe72
Komla Sanussi <your.kamilousanussi@gmail.com>
2026-03-17 21:08:16 +0000

    application of for loop

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,65 +1,11 @@
-import 'dart:async';
-
 void main() {
-  String days = 'Sunday'; // declaration of variable
+  List<double> dailyHarvests = [20.50, 12.6, 27.80, 90.38, 11.39, 10.0, 39.60];
+  double total = 0.0;
 
-//switch statement
-  switch (days) { 
-case 'Sunday':
-print ('Today is sunday. First day of the week. Go to church.');
-break;
-
-case 'Monday':
-print ( 'It is Monday. Second day of the week. Work begins.');
-break;
-
-case 'Tuesday':
-print('Another busy day. Third day of the week. Work continues.');
-break;
-
-case 'Wednesday':
-print('It is Wednesday. Mid-week, fourth day of the week. Another working day');
-break;
-
-case 'Thursday':
-print('Today is Thursday, the fith day of the week. The week is almost ended.');
-break;
-
-case 'Friday':
-print('It is Friday.The sixth day of the week. The last working day.');
-break;
-
-case 'Saturdy':
-print('It is Saturday. The last day of the week. It is weekend. Time for relaxation.');
-break;
-
-default:
-print('Unknown day. Please, check your input.'); 
-
-
-/* if else statement
-
-  int dayNumber = 3; 
-
-  if (dayNumber == 1) {
-      print('Monday: Water all seedlings.');
+  for (int i = 0; i < dailyHarvests.length; i++) {
+    total += dailyHarvests[i];
+    print('day ${i + 1} : ${dailyHarvests[i]} kg, total = $total kg ');
   }
-    else if (dayNumber == 2) {
-      print('Tuesday: Check for pests.');
-    }
-    else if (dayNumber == 3) {
-      print('Wednesday: Apply fertilizer.');
-    }
-    else if (dayNumber == 4) {
-      print('Thursday: Inspect irrigation system.');
-    }
-     else if (dayNumber == 5) {
-      print('Friday: Record daily yields.');
-     }
-     else {
-      print('Weekend: Rest and review the week.');
-      */
-     
-  }
+
+  print('Final total: $total kg ');
 }
-  
