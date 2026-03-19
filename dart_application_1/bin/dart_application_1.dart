17818a17c394a95a3bcaad9e11db0d1533aa9bae
Komla Sanussi <your.kamilousanussi@gmail.com>
2026-03-19 20:48:30 +0000

    Application of while loop

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,11 +1,14 @@
 void main() {
-  List<double> dailyHarvests = [20.50, 12.6, 27.80, 90.38, 11.39, 10.0, 39.60];
-  double total = 0.0;
+  
+  double plantHeight = 50.0;
+  int days = 0;
 
-  for (int i = 0; i < dailyHarvests.length; i++) {
-    total += dailyHarvests[i];
-    print('day ${i + 1} : ${dailyHarvests[i]} kg, total = $total kg ');
+  //Grow untill plant reaches 150
+
+  while(plantHeight < 150) {
+    plantHeight+=10;
+    days++;
+    print('day $days: Height = ${plantHeight} cm');
   }
-
-  print('Final total: $total kg ');
+  print('Plant fully grown in ${days} days');
 }
